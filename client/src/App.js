b81dff7fab4c90b681e4e1f9988c9cba6520b5b2
Sam Oberg <samueloberg@hotmail.com>
2021-06-26 12:09:06 -0700

    login page JSX complete

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
                     <Route exact path='/login' component={ Login } />
                 </Switch>
             </div>
-        </Router>
+      </Router>
   );
 }
 
