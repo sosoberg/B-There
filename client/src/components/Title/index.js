97798f7c76ff106520914bc26c045a93f0f952fe
Sam Oberg <samueloberg@hotmail.com>
2021-07-12 19:20:22 -0700

    styling fix

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -3,8 +3,12 @@ import './style.css';
 
 const Title = () => {
     return (
-        <div className="titleDiv">
-            <h1 className="headH1"><span className="B">B</span><span className='there'> there!</span></h1>
+        <div>
+            <div className="empty">
+            </div>
+            <div className="titleDiv">
+                <h1 className="headH1"><span className="B">B</span><span className='there'> there!</span></h1>
+            </div>
         </div>
     )
 }
